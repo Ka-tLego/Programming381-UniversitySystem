b199dd59994d373fd4bca8aca674df8bd8c15238
Ka-tLego <602023@student.belgiumcampus.ac.za>
2026-07-12 18:08:33 +0200

    Initial project skeleton - empty framework
    - Created a basic package structure
    - Added Main.java with minimal placeholder
    - Added .gitignore for NetBeans
    - Added README.md with project overview

---
+++ UniversityCleaningInventorySystem/src/com/university/cleaningInventory/Main.java
@@ -0,0 +1,7 @@
+package com.university.cleaningInventory;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("University Cleaning Inventory System");
+    }
+}
